d29dd55d084be3fb45615405e2ec2364b76c2275
Sivuyile Sifuba <sivuyilesifuba@gmail.com>
2023-12-08 15:15:38 +0200

    Added a CREATE TABLE script for all faculties in the university.

---
+++ CREATE TABLE faculties.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `faculties` (
+  `faculty_id` varchar(5) NOT NULL,
+  `faculty` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`faculty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
